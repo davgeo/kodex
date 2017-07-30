d0f8697a171494db88924cb8c0cd8702c564e0b2
David George <dcg.git@gmail.com>
2017-07-30 22:47:17 +0100

    Skip app selection screen

--- kodex/kodex/urls.py
+++ kodex/kodex/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from . import views
+from kodidj import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', views.kodi, name='index'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^kodi/', include('kodidj.urls'))
 ]
